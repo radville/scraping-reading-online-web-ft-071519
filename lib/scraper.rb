2feef234c8f177e632348e3a990f1cdecf5e3255
Laura Radville <laura.radville@gmail.com>
2019-07-31 20:06:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,3 +5,9 @@ html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(open("http://flatironschool.com/"))
 doc.css(".site-header__hero__headline").text.strip
+
+courses = doc.css(".tout__label.heading.heading--level-4")
+ 
+courses.each do |course|
+  puts course.text.strip
+end
